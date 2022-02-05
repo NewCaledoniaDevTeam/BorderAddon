8b26bc36f9baa4d01cd4066a54833e06a3f8f01b
PiSaucer <PiSaucer@gmail.com>
2022-02-04 22:33:30 -0600

    Update compress-json.sh

--- compress-json.sh
+++ compress-json.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+for file in borderbp/**/**/*.json; do
+    echo $(cat $file | grep -v "^\s*//") > $file
+    jq -c < "$file" > "$file"
+done
+
 for file in borderbp/**/*.json; do
+    echo $(cat $file | grep -v "^\s*//") > $file
+    jq -c < "$file" > "$file"
+done
+
+for file in borderrp/**/**/*.json; do
+    echo $(cat $file | grep -v "^\s*//") > $file
+    jq -c < "$file" > "$file"
+done
+
+for file in borderrp/**/*.json; do
     echo $(cat $file | grep -v "^\s*//") > $file
     jq -c < "$file" > "$file"
 done
