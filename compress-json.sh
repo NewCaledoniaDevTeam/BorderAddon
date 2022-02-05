a447a6ad29f6475e9a585ac0a16f7f550633814c
PiSaucer <PiSaucer@gmail.com>
2022-02-04 22:56:55 -0600

    Fix Compressed Json

--- compress-json.sh
+++ compress-json.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-for file in borderbp/**/**/*.json; do
+for file in borderbp/*.json; do
     echo $(cat $file | grep -v "^\s*//") > $file
-    jq -c < "$file" > "$file"
+    echo $(cat $file | jq -c .) > $file
 done
 
 for file in borderbp/**/*.json; do
     echo $(cat $file | grep -v "^\s*//") > $file
-    jq -c < "$file" > "$file"
+    echo $(cat $file | jq -c .) > $file
 done
 
-for file in borderrp/**/**/*.json; do
+for file in borderrp/*.json; do
     echo $(cat $file | grep -v "^\s*//") > $file
-    jq -c < "$file" > "$file"
+    echo $(cat $file | jq -c .) > $file
 done
 
 for file in borderrp/**/*.json; do
     echo $(cat $file | grep -v "^\s*//") > $file
-    jq -c < "$file" > "$file"
+    echo $(cat $file | jq -c .) > $file
 done
